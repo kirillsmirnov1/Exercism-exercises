3ac500bf776fe8d3cdd12078085d4b5ab34db736
Kirill Smirnov <thegaldren@gmail.com>
2020-08-16 17:15:30 +0300

    Add CircularBuffer.cs

---
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CircularBuffer<T>
+{
+    private readonly Queue<T> _buffer;
+    private readonly int _maxCapacity;
+    
+    public CircularBuffer(int capacity)
+    {
+        _buffer = new Queue<T>();
+        _maxCapacity = capacity;
+    }
+
+    public T Read() => _buffer.Dequeue();
+
+    public void Write(T value)
+    {
+        if(_buffer.Count == _maxCapacity) 
+            throw new InvalidOperationException();
+        
+        _buffer.Enqueue(value);
+    }
+
+    public void Overwrite(T value)
+    {
+        if(_buffer.Count == _maxCapacity) _buffer.Dequeue();
+        _buffer.Enqueue(value);
+    }
+
+    public void Clear() => _buffer.Clear();
+}
