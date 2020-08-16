3c27b4d30c167b32e1b7dfb13bce879486016742
Kirill Smirnov <thegaldren@gmail.com>
2020-08-16 14:44:13 +0300

    RotationalCipher.cs

---
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+
+public static class RotationalCipher
+{
+    private const int AlphabetSize = 26;
+    public static string Rotate(string text, int shiftKey)
+        => text.Select(c => RotateChar(c, shiftKey)).Aggregate("", (a, b) => a + b);
+
+    private static char RotateChar(char c, int key)
+    {
+        if (c >= 'a' && c <= 'z')
+        {
+            return (char)('a' + RotateInt(c - 'a', key));
+        }
+
+        if (c >= 'A' && c <= 'Z')
+        {
+            return (char)('A' + RotateInt(c - 'A', key));
+        }
+
+        return c;
+    }
+
+    private static int RotateInt(int c, int key) => (c + key) % AlphabetSize;
+}
