b16617d09e2637ee0d541f611032e0639f5df3af
Kirill Smirnov <thegaldren@gmail.com>
2020-08-18 17:39:04 +0300

    Actually implement CircularBuffer

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -1,32 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
 
 public class CircularBuffer<T>
 {
-    private readonly Queue<T> _buffer;
+    private readonly T[] _buffer;
     private readonly int _maxCapacity;
+    private int _readPos = 0;
+    private int _writePos = 0;
+    private int _count = 0;
     
     public CircularBuffer(int capacity)
     {
-        _buffer = new Queue<T>();
+        _buffer = new T[capacity];
         _maxCapacity = capacity;
     }
 
-    public T Read() => _buffer.Dequeue();
+    public T Read()
+    {
+        if (_count == 0) throw new InvalidOperationException();
+        
+        var obj = _buffer[_readPos];
+        _readPos = CircularIncrement(_readPos);
+        _count--;
+
+        return obj;
+    }
 
     public void Write(T value)
     {
-        if(_buffer.Count == _maxCapacity) 
-            throw new InvalidOperationException();
-        
-        _buffer.Enqueue(value);
+        if(_count == _maxCapacity) throw new InvalidOperationException();
+
+        _buffer[_writePos] = value;
+        _writePos = CircularIncrement(_writePos);
+        _count++;
     }
 
     public void Overwrite(T value)
     {
-        if(_buffer.Count == _maxCapacity) _buffer.Dequeue();
-        _buffer.Enqueue(value);
+        if (_count < _maxCapacity)
+        {
+            Write(value);
+        }
+        else
+        {
+            _buffer[_readPos] = value;
+            _readPos = CircularIncrement(_readPos);
+        }
     }
 
-    public void Clear() => _buffer.Clear();
+    private int CircularIncrement(int value) => (value + 1) % _maxCapacity;
+
+    public void Clear()
+    {
+        _count = 0;
+        _readPos = _writePos;
+    }
 }
