69ace5636e3f438dacc9a9fbf7ba8c69e40de7ff
Kirill Smirnov <thegaldren@gmail.com>
2020-08-18 18:12:47 +0300

    CryptoSquare solution

--- csharp/crypto-square/CryptoSquare.cs
+++ csharp/crypto-square/CryptoSquare.cs
@@ -1,25 +1,61 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class CryptoSquare
 {
-    public static string NormalizedPlaintext(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public static IEnumerable<string> PlaintextSegments(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public static string Encoded(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
     public static string Ciphertext(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (plaintext.Length == 0) return "";
+        
+        string oneLiner = ToOneliner(plaintext, out int r, out int c);
+        char[][] charMatrix = ToCharMatrix(oneLiner, r, c);
+        char[][] cipheredMatrix = CipherMatrix(charMatrix);
+        
+        return MatrixToString(cipheredMatrix);
+    }
+
+    private static string MatrixToString(char[][] cipheredMatrix)
+        => string.Join(" ", cipheredMatrix.Select(x => new string(x)));
+
+    private static char[][] CipherMatrix(char[][] charMatrix)
+    {
+        char[][] m = new char[charMatrix[0].Length][];
+        for (int i = 0; i < m.Length; ++i)
+        {
+            m[i] = new char[charMatrix.Length];
+        }
+
+        for (int i = 0; i < charMatrix.Length; ++i)
+        {
+            for (int j = 0; j < charMatrix[0].Length; ++j)
+            {
+                m[j][i] = charMatrix[i][j];
+            }
+        }
+
+        return m;
+    }
+
+    private static char[][] ToCharMatrix(string oneLiner, int r, int c)
+        => Enumerable.Range(0, r)
+            .Select(rowI => oneLiner.Substring(rowI * c, c).ToCharArray())
+            .ToArray();
+
+    private static string ToOneliner(string plainText, out int r, out int c)
+    {
+        string characters = new string(plainText.ToLower().Where(char.IsLetterOrDigit).ToArray());
+        
+        c = (int)Math.Floor(Math.Sqrt(characters.Length));
+        r = c;
+
+        if (c * r < characters.Length) c++;
+        if (c * r < characters.Length) r++;
+        
+        return new string(
+            Enumerable
+                .Range(0, r * c - characters.Length)
+                .Aggregate(characters, (a, b) => a + ' ')
+                .ToArray());
     }
 }
