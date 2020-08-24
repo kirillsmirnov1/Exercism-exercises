ebf2a21a4fc8d97b28122d6e3a87fae399bd425a
Kirill Smirnov <thegaldren@gmail.com>
2020-08-24 15:46:55 +0300

    Add ErrorHandling

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException() => throw new Exception();
+
+    public static int? HandleErrorByReturningNullableType(string input)
+        => int.TryParse(input, out int res)
+            ? res
+            : (int?) null;
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+        => int.TryParse(input, out result);
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+        {
+            throw new Exception();
+        }
+    }
+}
